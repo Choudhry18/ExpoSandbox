6c01baebd1a3897e9e955e658662678750695fa6
Choudhry Abdullah <cabdulla@trinity.edu>
2024-07-15 02:58:37 -0500

    Fixed the header titles and back title

--- app/(tabs)/scan/_layout.tsx
+++ app/(tabs)/scan/_layout.tsx
@@ -4,9 +4,13 @@ import React from 'react';
 export default function _layout(){
     return(
         <Stack>
-            <Stack.Screen name='index'/>
+            <Stack.Screen 
+            name='index'
+            options={{ headerTitle: 'Scanner'}}
+            />
             <Stack.Screen 
             name='view'
+            options={{ headerBackTitle: 'Scan Again' }}
             />
         </Stack>
     )
